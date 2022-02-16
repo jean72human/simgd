28913e72ed5ad7c61ac15e06538fab007eaca402
Gbetondji Dovonon <jsadovonon@gmail.com>
2022-02-16 10:25:17 +0000

    add gcn model

---
+++ simgd/ghn/gnn/gcn.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+
+class GCNModel(nn.Module):
+    def __init__(self, num_layers=2, sz_in=1433, sz_hid=32, sz_out=7):
+        super().__init__()
+
+        ### BEGIN SOLUTION
+        self.hidden = nn.Linear(sz_in,sz_hid, bias=False)
+        self.out = nn.Linear(sz_hid,sz_out, bias=False)
+        self.ReLU = nn.ReLU()
+        ### END SOLUTION
+
+    def forward(self,  x, edges, node_graph_ind):
+        """
+        Args:
+            fts: [N, 1433]
+            adj: [N, N]
+
+        Returns:
+            new_fts: [N, 7]
+        """
+        adj = torch.zeros((x.size(0),x.size(0)), device=x.device)
+        for ind in edges[:,:2]: 
+            adj[ind[0],ind[1]]=1
+            adj[ind[1],ind[0]]=1
+        deg = adj.sum(axis=1, keepdim=True) # Degree of nodes, shape [N, 1]
+
+        ### BEGIN SOLUTION
+        x = self.ReLU(self.hidden(1/deg * adj @ x))
+        x = self.out(1/deg * adj @ x)
+        ### END SOLUTION
+        return x
--- simgd/ghn/nn.py
+++ simgd/ghn/nn.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import os
 #from .mlp import MLP
 from .gnn.gatedgnn import GatedGNN
+from .gnn.gcn import GCNModel
 from .decoder import MLPDecoder, ConvDecoder
 from .encoder import MLPEncoder, ConvEncoder
 from .layers import ShapeEncoder
@@ -49,6 +50,8 @@ class GHN(nn.Module):
 
         if hypernet == 'gatedgnn':
             self.gnn = GatedGNN(in_features=hid*2, ve=ve)
+        elif hypernet == 'gcn':
+            self.gnn = GCNModel(sz_in=hid*2,sz_out=hid*2)
         else:
             raise NotImplementedError(hypernet)
         #elif hypernet == 'mlp':
