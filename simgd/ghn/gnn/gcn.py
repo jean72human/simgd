dd168181d8afa3e60f5db80f68a7ab9b98f84310
Gbetondji Dovonon <jsadovonon@gmail.com>
2022-02-22 23:08:11 +0000

    gcn upgraded

--- simgd/ghn/gnn/gcn.py
+++ simgd/ghn/gnn/gcn.py
@@ -6,8 +6,10 @@ class GCNModel(nn.Module):
         super().__init__()
 
         ### BEGIN SOLUTION
-        self.hidden = nn.Linear(sz_in,sz_hid, bias=False)
-        self.out = nn.Linear(sz_hid,sz_out, bias=False)
+        self.hidden1 = nn.Linear(sz_in,sz_hid, bias=False)
+        self.out1 = nn.Linear(sz_hid,sz_out, bias=False)
+        self.hidden2 = nn.Linear(sz_in,sz_hid, bias=False)
+        self.out2 = nn.Linear(sz_hid,sz_out, bias=False)
         self.ReLU = nn.ReLU()
         ### END SOLUTION
 
@@ -20,14 +22,20 @@ class GCNModel(nn.Module):
         Returns:
             new_fts: [N, 7]
         """
-        adj = torch.zeros((x.size(0),x.size(0)), device=x.device)
+        adj1 = torch.zeros((x.size(0),x.size(0)), device=x.device)
         for ind in edges[:,:2]: 
-            adj[ind[0],ind[1]]=1
-            adj[ind[1],ind[0]]=1
-        deg = adj.sum(axis=1, keepdim=True) # Degree of nodes, shape [N, 1]
+            adj1[ind[0],ind[1]]=1
+            #adj[ind[1],ind[0]]=1
+
+        deg1 = adj1.sum(axis=1, keepdim=True) # Degree of nodes, shape [N, 1]
+        adj2 = adj1.T
+        deg2 = adj2.sum(axis=1, keepdim=True)
 
         ### BEGIN SOLUTION
-        x = self.ReLU(self.hidden(1/deg * adj @ x))
-        x = self.out(1/deg * adj @ x)
+        x1 = self.ReLU(self.hidden1(1/deg1 * adj1 @ x))
+        x1 = self.out1(1/deg1 * adj1 @ x1)
+
+        x2 = self.ReLU(self.hidden2(1/deg2 * adj2 @ x))
+        x2 = self.out2(1/deg1 * adj2 @ x2)
         ### END SOLUTION
-        return x
+        return (x1 + x2)/2
